ec44a9347ee37b18c42976e4c1976778ae693f28
Cory McKiel <c_mckiel@outlook.com>
2025-07-01 23:56:10 +0000

    a couple basic init tests

--- platforms/stm32f446re/hal/uart/src/stm32f4_uart.c
+++ platforms/stm32f446re/hal/uart/src/stm32f4_uart.c
@@ -6,9 +6,9 @@
 #endif
 
 #include "uart.h"
-#include "stm32f4_uart1.h"
-#include "stm32f4_uart2.h"
 #include "circular_buffer.h"
+// #include "stm32f4_uart1.h"
+// #include "stm32f4_uart2.h"
 
 typedef struct {
     USART_TypeDef *USARTx;
@@ -18,6 +18,8 @@ typedef struct {
 	size_t GPIOxEN;
 	size_t USARTxEN;
 	size_t USARTx_IRQn;
+	size_t rx_buffer_size;
+	size_t tx_buffer_size;
 } stm32f4_uart_t;
 
 static stm32f4_uart_t uart1 = {
@@ -28,6 +30,8 @@ static stm32f4_uart_t uart1 = {
 	.GPIOxEN = 0, // @todo fill in the clock enable for gpio port.
 	.USARTxEN = RCC_APB2ENR_USART1EN,
 	.USARTx_IRQn = USART1_IRQn,
+	.rx_buffer_size = CIRCULAR_BUFFER_MAX_SIZE,
+	.tx_buffer_size = CIRCULAR_BUFFER_MAX_SIZE,
 };
 
 static stm32f4_uart_t uart2 = {
@@ -38,7 +42,69 @@ static stm32f4_uart_t uart2 = {
 	.GPIOxEN = RCC_AHB1ENR_GPIOAEN,
 	.USARTxEN = RCC_APB1ENR_USART2EN,
 	.USARTx_IRQn = USART2_IRQn,
+	.rx_buffer_size = CIRCULAR_BUFFER_MAX_SIZE,
+	.tx_buffer_size = CIRCULAR_BUFFER_MAX_SIZE,
 };
+
+HalStatus_t hal_uart_init(HalUart_t uart, void *config)
+{
+	HalStatus_t hal_status = HAL_STATUS_OK;
+
+	if (!circular_buffer_init(&uart2.rx_ctx, uart2.rx_buffer_size))
+	{
+		hal_status = HAL_STATUS_ERROR;
+	}
+
+	if (!circular_buffer_init(&uart2.tx_ctx, uart2.tx_buffer_size))
+	{
+		hal_status = HAL_STATUS_ERROR;
+	}
+
+	RCC->AHB1ENR |= uart2.GPIOxEN;
+
+	return hal_status;
+}
+
+HalStatus_t hal_uart_write(HalUart_t uart, const uint8_t *data, size_t len)
+{
+	HalStatus_t hal_status = HAL_STATUS_ERROR;
+	return hal_status;
+}
+
+HalStatus_t hal_uart_read(HalUart_t uart, uint8_t *data, size_t len, size_t *bytes_read, uint32_t timeout_ms)
+{
+	HalStatus_t hal_status = HAL_STATUS_ERROR;
+	return hal_status;
+}
+
+#ifdef SIMULATION_BUILD
+
+void _hal_uart_inject_rx_buffer_failure(HalUart_t uart)
+{
+	switch (uart)
+	{
+		case HAL_UART1:
+			uart1.rx_buffer_size = CIRCULAR_BUFFER_MAX_SIZE + 1;
+		break;
+		case HAL_UART2:
+			uart2.rx_buffer_size = CIRCULAR_BUFFER_MAX_SIZE + 1;
+		break;
+	}
+}
+
+void _hal_uart_inject_tx_buffer_failure(HalUart_t uart)
+{
+	switch (uart)
+	{
+		case HAL_UART1:
+			uart1.tx_buffer_size = CIRCULAR_BUFFER_MAX_SIZE + 1;
+		break;
+		case HAL_UART2:
+			uart2.tx_buffer_size = CIRCULAR_BUFFER_MAX_SIZE + 1;
+		break;
+	}
+}
+#endif
 
 /**
  * @brief Redefine the putchar() function.
@@ -52,64 +118,4 @@ int __io_putchar(int ch)
 	hal_uart_write(HAL_UART2, &data[0], len);
 
 	return ch;
-}
-
-HalStatus_t hal_uart_init(HalUart_t uart, void *config)
-{
-	HalStatus_t hal_status = HAL_STATUS_ERROR;
-
-	if (uart == HAL_UART1)
-	{
-		stm32f4_uart1_init(config);
-	}
-	else if (uart == HAL_UART2)
-	{
-		stm32f4_uart2_init(config);
-	}
-	else if (uart == HAL_UART3)
-	{
-		// Not implemented.
-	}
-
-	return hal_status;
-}
-
-HalStatus_t hal_uart_write(HalUart_t uart, const uint8_t *data, size_t len)
-{
-	HalStatus_t hal_status = HAL_STATUS_ERROR;
-
-	if (uart == HAL_UART1)
-	{
-		hal_status = stm32f4_uart1_write(data, len);
-	}
-	else if (uart == HAL_UART2)
-	{
-		hal_status = stm32f4_uart2_write(data, len);
-	}
-	else if (uart == HAL_UART3)
-	{
-		// Not implemented.
-	}
-
-	return hal_status;
-}
-
-HalStatus_t hal_uart_read(HalUart_t uart, uint8_t *data, size_t len, size_t *bytes_read, uint32_t timeout_ms)
-{
-	HalStatus_t hal_status = HAL_STATUS_ERROR;
-
-	if (uart == HAL_UART1)
-	{
-		hal_status = stm32f4_uart1_read(data, len, timeout_ms);
-	}
-	else if (uart == HAL_UART2)
-	{
-		hal_status = stm32f4_uart2_read(data, len, bytes_read, timeout_ms);
-	}
-	else if (uart == HAL_UART3)
-	{
-		// Not implemented.
-	}
-
-	return hal_status;
 }
--- simulations/stm32f446re/test/uart2_driver_test.cpp
+++ simulations/stm32f446re/test/uart2_driver_test.cpp
@@ -1,8 +1,7 @@
 #include "gtest/gtest.h"
 
 extern "C" {
-#include "stm32f4_uart2.h"
-#include "stm32f4_hal.h"
+#include "uart.h"
 #include "registers.h"
 #include "nvic.h"
 }
@@ -10,7 +9,11 @@ extern "C" {
 // Allow test to directly call the ISR
 extern "C" void USART2_IRQHandler(void);
 
-class UartDriverTest : public ::testing::Test {
+// Allow test access to UART internals.
+extern "C" void _hal_uart_inject_rx_buffer_failure(HalUart_t);
+extern "C" void _hal_uart_inject_tx_buffer_failure(HalUart_t);
+
+class Uart2DriverTest : public ::testing::Test {
 protected:
     void SetUp() override {
         // Clear peripheral state before each test
@@ -20,87 +23,137 @@ protected:
     }
 };
 
-TEST_F(UartDriverTest, InitSetsRegistersCorrectly) {
-    ASSERT_EQ(stm32f4_uart2_init(nullptr), HAL_STATUS_OK);
-    EXPECT_TRUE(Sim_USART2.CR1 & USART_CR1_UE);
-    EXPECT_TRUE(Sim_USART2.CR1 & USART_CR1_RE);
-    EXPECT_TRUE(Sim_USART2.CR1 & USART_CR1_TE);
-}
-
-TEST_F(UartDriverTest, SimulateRxInterrupt) {
-    stm32f4_uart2_init(nullptr);
-
-    Sim_USART2.DR = 'A';
-    Sim_USART2.SR |= USART_SR_RXNE;
-    USART2_IRQHandler();  // simulate interrupt
-
-    uint8_t buf[1];
-    size_t bytes_read = 0;
-    ASSERT_EQ(stm32f4_uart2_read(buf, 1, &bytes_read, 0), HAL_STATUS_OK);
-    EXPECT_EQ(bytes_read, 1);
-    EXPECT_EQ(buf[0], 'A');
-}
-
-TEST_F(UartDriverTest, SimulateTxInterrupt) {
-    stm32f4_uart2_init(nullptr);
-
-    uint8_t data = 'B';
-    ASSERT_EQ(stm32f4_uart2_write(&data, 1), HAL_STATUS_OK);
-
-    // Simulate TXE interrupt trigger
-    Sim_USART2.SR |= USART_SR_TXE;
-    USART2_IRQHandler();
-
-    EXPECT_EQ(Sim_USART2.DR, 'B');
-}
-
-TEST_F(UartDriverTest, WriteEnablesTXEInterruptForEmptyBuffer)
+TEST_F(Uart2DriverTest, InitSuceeds)
 {
-    stm32f4_uart2_init(nullptr);
-
-    // Assert the interrupt is not enabled.
-    ASSERT_EQ(Sim_USART2.CR1 & USART_CR1_TXEIE, 0);
-
-    // Write data.
-    uint8_t data[4] = { 0, 1, 2, 3 };
-    ASSERT_EQ(stm32f4_uart2_write(&data[0], sizeof(data)), HAL_STATUS_OK);
-
-    // Assert the interrupt is enabled.
-    ASSERT_EQ((Sim_USART2.CR1 & USART_CR1_TXEIE), USART_CR1_TXEIE);
+    HalStatus_t status;
+    status = hal_uart_init(HAL_UART2, nullptr);
+    ASSERT_EQ(status, HAL_STATUS_OK);
 }
 
-TEST_F(UartDriverTest, ISRDisablesTXEInterruptForEmptyBuffer)
+TEST_F(Uart2DriverTest, InitFailsForRxBufferInitFailure)
 {
-    /******* SETUP **********/
-    stm32f4_uart2_init(nullptr);
+    HalStatus_t status;
 
-    // Assert the interrupt is not enabled.
-    ASSERT_EQ(Sim_USART2.CR1 & USART_CR1_TXEIE, 0);
+    // Arrange
+    _hal_uart_inject_rx_buffer_failure(HAL_UART2);
 
-    // Write data.
-    uint8_t data[4] = { 0, 1, 2, 3 };
-    ASSERT_EQ(stm32f4_uart2_write(&data[0], sizeof(data)), HAL_STATUS_OK);
+    // Act
+    status = hal_uart_init(HAL_UART2, nullptr);
 
-    // Assert the interrupt is enabled.
-    ASSERT_EQ((Sim_USART2.CR1 & USART_CR1_TXEIE), USART_CR1_TXEIE);
-
-    /******* TEST **********/
-    // Hardware becomes available for transmit.
-    Sim_USART2.SR |= USART_SR_TXE;
-
-    USART2_IRQHandler();
-    ASSERT_EQ(Sim_USART2.DR, 0);
-
-    USART2_IRQHandler();
-    ASSERT_EQ(Sim_USART2.DR, 1);
-
-    USART2_IRQHandler();
-    ASSERT_EQ(Sim_USART2.DR, 2);
-
-    USART2_IRQHandler();
-    ASSERT_EQ(Sim_USART2.DR, 3);
-
-    USART2_IRQHandler();
-    // Assert the interrupt is not enabled.
-    ASSERT_EQ(Sim_USART2.CR1 & USART_CR1_TXEIE, 0);
+    // Assert
+    ASSERT_EQ(status, HAL_STATUS_ERROR);
 }
+
+TEST_F(Uart2DriverTest, InitFailsForTxBufferInitFailure)
+{
+    HalStatus_t status;
+
+    // Arrange
+    _hal_uart_inject_tx_buffer_failure(HAL_UART2);
+
+    // Act
+    status = hal_uart_init(HAL_UART2, nullptr);
+
+    // Assert
+    ASSERT_EQ(status, HAL_STATUS_ERROR);
+}
+
+TEST_F(Uart2DriverTest, EnablesGpioClock)
+{
+    HalStatus_t status;
+
+    // Arrange
+    ASSERT_EQ(Sim_RCC.AHB1ENR, 0);
+
+    // Act
+    status = hal_uart_init(HAL_UART2, nullptr);
+
+    // Assert
+    ASSERT_EQ(status, HAL_STATUS_OK);
+    ASSERT_EQ(Sim_RCC.AHB1ENR & RCC_AHB1ENR_GPIOAEN, RCC_AHB1ENR_GPIOAEN);
+}
+
+// TEST_F(UartDriverTest, InitSetsRegistersCorrectly) {
+//     ASSERT_EQ(stm32f4_uart2_init(nullptr), HAL_STATUS_OK);
+//     EXPECT_TRUE(Sim_USART2.CR1 & USART_CR1_UE);
+//     EXPECT_TRUE(Sim_USART2.CR1 & USART_CR1_RE);
+//     EXPECT_TRUE(Sim_USART2.CR1 & USART_CR1_TE);
+// }
+
+// TEST_F(UartDriverTest, SimulateRxInterrupt) {
+//     stm32f4_uart2_init(nullptr);
+
+//     Sim_USART2.DR = 'A';
+//     Sim_USART2.SR |= USART_SR_RXNE;
+//     USART2_IRQHandler();  // simulate interrupt
+
+//     uint8_t buf[1];
+//     size_t bytes_read = 0;
+//     ASSERT_EQ(stm32f4_uart2_read(buf, 1, &bytes_read, 0), HAL_STATUS_OK);
+//     EXPECT_EQ(bytes_read, 1);
+//     EXPECT_EQ(buf[0], 'A');
+// }
+
+// TEST_F(UartDriverTest, SimulateTxInterrupt) {
+//     stm32f4_uart2_init(nullptr);
+
+//     uint8_t data = 'B';
+//     ASSERT_EQ(stm32f4_uart2_write(&data, 1), HAL_STATUS_OK);
+
+//     // Simulate TXE interrupt trigger
+//     Sim_USART2.SR |= USART_SR_TXE;
+//     USART2_IRQHandler();
+
+//     EXPECT_EQ(Sim_USART2.DR, 'B');
+// }
+
+// TEST_F(UartDriverTest, WriteEnablesTXEInterruptForEmptyBuffer)
+// {
+//     stm32f4_uart2_init(nullptr);
+
+//     // Assert the interrupt is not enabled.
+//     ASSERT_EQ(Sim_USART2.CR1 & USART_CR1_TXEIE, 0);
+
+//     // Write data.
+//     uint8_t data[4] = { 0, 1, 2, 3 };
+//     ASSERT_EQ(stm32f4_uart2_write(&data[0], sizeof(data)), HAL_STATUS_OK);
+
+//     // Assert the interrupt is enabled.
+//     ASSERT_EQ((Sim_USART2.CR1 & USART_CR1_TXEIE), USART_CR1_TXEIE);
+// }
+
+// TEST_F(UartDriverTest, ISRDisablesTXEInterruptForEmptyBuffer)
+// {
+//     /******* SETUP **********/
+//     stm32f4_uart2_init(nullptr);
+
+//     // Assert the interrupt is not enabled.
+//     ASSERT_EQ(Sim_USART2.CR1 & USART_CR1_TXEIE, 0);
+
+//     // Write data.
+//     uint8_t data[4] = { 0, 1, 2, 3 };
+//     ASSERT_EQ(stm32f4_uart2_write(&data[0], sizeof(data)), HAL_STATUS_OK);
+
+//     // Assert the interrupt is enabled.
+//     ASSERT_EQ((Sim_USART2.CR1 & USART_CR1_TXEIE), USART_CR1_TXEIE);
+
+//     /******* TEST **********/
+//     // Hardware becomes available for transmit.
+//     Sim_USART2.SR |= USART_SR_TXE;
+
+//     USART2_IRQHandler();
+//     ASSERT_EQ(Sim_USART2.DR, 0);
+
+//     USART2_IRQHandler();
+//     ASSERT_EQ(Sim_USART2.DR, 1);
+
+//     USART2_IRQHandler();
+//     ASSERT_EQ(Sim_USART2.DR, 2);
+
+//     USART2_IRQHandler();
+//     ASSERT_EQ(Sim_USART2.DR, 3);
+
+//     USART2_IRQHandler();
+//     // Assert the interrupt is not enabled.
+//     ASSERT_EQ(Sim_USART2.CR1 & USART_CR1_TXEIE, 0);
+// }

83b1353ce7808b9381eff2531d9a35eaa73edc36
Cory McKiel <c_mckiel@outlook.com>
2025-07-05 01:14:02 +0000

    cool uart tunnel

--- test/main.c
+++ test/main.c
@@ -14,27 +14,40 @@
  */
 int main(void)
 {
-	size_t bytes_read = 0;
-	uint8_t rx_data[MAX_RX_BYTES] = { 0 };
+	size_t bytes_read_uart1 = 0;
+	size_t bytes_read_uart2 = 0;
+	uint8_t rx_data_uart1[MAX_RX_BYTES] = { 0 };
+	uint8_t rx_data_uart2[MAX_RX_BYTES] = { 0 };
 
 	hal_gpio_init(NULL);
 	hal_uart_init(HAL_UART1, NULL);
+	hal_uart_init(HAL_UART2, NULL);
 
 	while (1)
 	{
 		hal_delay_ms(100);
 
-		bytes_read = 0;
-		hal_uart_read(HAL_UART1, &rx_data[0], sizeof(rx_data), &bytes_read, 0);
+		bytes_read_uart1 = 0;
+		bytes_read_uart2 = 0;
+		hal_uart_read(HAL_UART1, &rx_data_uart1[0], sizeof(rx_data_uart1), &bytes_read_uart1, 0);
+		hal_uart_read(HAL_UART2, &rx_data_uart2[0], sizeof(rx_data_uart2), &bytes_read_uart2, 0);
 
 		// Solves a weird newline issue.
-		if (rx_data[bytes_read-1] == '\r')
+		if (rx_data_uart1[bytes_read_uart1-1] == '\r')
 		{
-			rx_data[bytes_read] = '\n';
-			bytes_read += 1;
+			rx_data_uart1[bytes_read_uart1] = '\n';
+			bytes_read_uart1 += 1;
 		}
 
-		hal_uart_write(HAL_UART1, &rx_data[0], bytes_read);
+		// Solves a weird newline issue.
+		if (rx_data_uart2[bytes_read_uart2-1] == '\r')
+		{
+			rx_data_uart2[bytes_read_uart2] = '\n';
+			bytes_read_uart2 += 1;
+		}
+
+		hal_uart_write(HAL_UART1, &rx_data_uart2[0], bytes_read_uart2);
+		hal_uart_write(HAL_UART2, &rx_data_uart1[0], bytes_read_uart1);
 	}
 
 	return 0;

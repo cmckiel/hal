f37ec4ae7360d5204fe6f825db3406da16e7ff6f
Cory McKiel <c_mckiel@outlook.com>
2025-08-08 11:41:13 +0000

    message queue

--- platforms/stm32f446re/hal/i2c/src/i2c_message_queue.c
+++ platforms/stm32f446re/hal/i2c/src/i2c_message_queue.c
@@ -3,6 +3,17 @@
 
 static circular_buffer_ctx event_queue;
 
+#define I2C_MESSAGE_QUEUE_SIZE 10
+
+typedef struct {
+    i2c_message_t messages[I2C_MESSAGE_QUEUE_SIZE];
+    size_t head;
+    size_t tail; // the current message
+    size_t message_count;
+} i2c_message_queue_t;
+
+static i2c_message_queue_t queue = { .messages = {0}, .head = 0, .tail = 0, .message_count = 0 };
+
 void message_queue_init()
 {
     circular_buffer_init(&event_queue, 32);
@@ -10,12 +21,54 @@ void message_queue_init()
 
 i2c_queue_status_t add_message_to_queue(i2c_message_t *message)
 {
-    return I2C_QUEUE_STATUS_SUCCESS;
+    i2c_queue_status_t status = I2C_QUEUE_STATUS_FAIL;
+
+    if (message)
+    {
+        if (queue.message_count < I2C_MESSAGE_QUEUE_SIZE)
+        {
+            queue.messages[queue.head] = *message;
+            queue.head = (queue.head + 1) % I2C_MESSAGE_QUEUE_SIZE;
+            queue.message_count++;
+
+            status = I2C_QUEUE_STATUS_SUCCESS;
+        }
+        else
+        {
+            status = I2C_QUEUE_STATUS_QUEUE_FULL;
+        }
+    }
+
+    return status;
 }
 
 i2c_queue_status_t get_current_message_from_queue(i2c_message_t *message)
 {
-    return I2C_QUEUE_STATUS_SUCCESS;
+    i2c_queue_status_t status = I2C_QUEUE_STATUS_FAIL;
+
+    if (message)
+    {
+        if (queue.message_count == 0)
+        {
+            status = I2C_QUEUE_STATUS_QUEUE_EMPTY;
+        }
+        else
+        {
+            *message = queue.messages[queue.tail];
+            status = I2C_QUEUE_STATUS_SUCCESS;
+        }
+    }
+
+    return status;
+}
+
+void next_message_in_queue()
+{
+    if (queue.head != queue.tail)
+    {
+        queue.tail = (queue.tail + 1) % I2C_MESSAGE_QUEUE_SIZE;
+        queue.message_count--;
+    }
 }
 
 bool update_current_message_state(i2c_message_processing_event_t event)
@@ -26,11 +79,6 @@ bool update_current_message_state(i2c_message_processing_event_t event)
     //      transition to fault state. abort message transmission.
     // }
     return true;
-}
-
-void next_message_in_queue()
-{
-    return;
 }
 
 void enqueue_event(i2c_message_processing_event_t event)

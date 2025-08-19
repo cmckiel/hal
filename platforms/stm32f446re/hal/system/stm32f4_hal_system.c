a3a1ee9a68c7e611af1ae881d89febfa1a99d6c9
Cory McKiel <c_mckiel@outlook.com>
2025-08-19 13:38:10 +0000

    fix for static analysis build

--- platforms/stm32f446re/hal/system/stm32f4_hal_system.c
+++ platforms/stm32f446re/hal/system/stm32f4_hal_system.c
@@ -1,5 +1,4 @@
 #include "hal_system.h"
-#include "stm32f4xx.h"
 
 // Coprocessor full access enables full use of the Floating Point Unit (FPU).
 #define CP_FULL_ACCESS 3UL
@@ -9,14 +8,26 @@
 #define CP10 20
 #define CP11 22
 
-void hal_system_init()
+#ifdef SIMULATION_BUILD
+
+void SystemInit(void)
 {
-    SystemInit();
+    return;
 }
+
+#else
+#include "stm32f4xx.h"
 
 void SystemInit(void)
 {
     // Give CP10 & CP11 full access (FPU)
     SCB->CPACR |= (CP_FULL_ACCESS << CP10) | (CP_FULL_ACCESS << CP11);
     __DSB(); __ISB();  // complete prior writes & flush pipeline
+}
+
+#endif
+
+void hal_system_init()
+{
+    SystemInit();
 }

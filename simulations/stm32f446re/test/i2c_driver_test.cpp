8d228b1e4b1b443af09868bf7e580db6b26eee44
Cory McKiel <c_mckiel@outlook.com>
2025-09-19 15:14:09 +0000

    cleanup

--- simulations/stm32f446re/test/i2c_driver_test.cpp
+++ simulations/stm32f446re/test/i2c_driver_test.cpp
@@ -6,46 +6,27 @@ extern "C" {
 #include "nvic.h"
 #include "stm32f4_hal.h"
 #include "circular_buffer.h"
-}
 
-extern "C" {
-    void I2C1_ER_IRQHandler(void);
-    void I2C1_EV_IRQHandler(void);
-    void _test_fixture_hal_i2c_reset_internals();
+void I2C1_ER_IRQHandler(void);
+void I2C1_EV_IRQHandler(void);
+void _test_fixture_hal_i2c_reset_internals();
 }
-
-// Helper for random test data.
-// uint8_t random_uint8() {
-//     return (uint8_t)(rand() % 256);
-// }
 
 class I2CDriverTest : public ::testing::Test {
-private:
-    // static bool seed_is_set;
-    // int best_seed_ever = 42;
 protected:
     void SetUp() override {
         // Clear peripheral state before each test
-        // @todo Sim I2C
         Sim_GPIOB = {0};
         Sim_RCC = {0};
         Sim_I2C1 = {0};
 
         _test_fixture_hal_i2c_reset_internals();
-
-        // Set up the seed only once per full testing run.
-        // if (!seed_is_set) {
-        //     srand(best_seed_ever);
-        //     seed_is_set = true;
-        // }
     }
 
     void TearDown() override {
         hal_i2c_deinit();
     }
 };
-
-// bool I2CDriverTest::seed_is_set = false;
 
 /***********************************************/
 // Init Tests

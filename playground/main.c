7571ebeff44717c5a51b932bfa167a3f0449bf18
Cory McKiel <c_mckiel@outlook.com>
2025-12-04 16:36:48 -0600

    Revert "testing for clean (need to revert this commit)"
    This reverts commit 06683411f7328e6783499b47d44072074bd8a75c.

--- playground/main.c
+++ playground/main.c
@@ -1,26 +1,11 @@
 #include "hal_system.h"
-#include "hal_metadata.h"
 #include "i2c.h"
 #include "pwm.h"
 #include "uart.h"
 #include "systick.h"
 
-#include <string.h>
-#include <stdio.h>
-
 int main(void)
 {
-    const hal_metadata_t* hal_metadata = hal_get_metadata();
-
-    hal_uart_init(HAL_UART2);
-
-    printf("Using HAL v%s (git %s [%s], built %s)\r\n",
-        hal_metadata->version_str,
-        hal_metadata->git_hash,
-        hal_metadata->dirty_str,
-        hal_metadata->build_date);
-
-
     // Super loop
     while (1)
     {
